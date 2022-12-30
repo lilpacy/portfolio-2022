0829f78c188142ba67cff649cf4a7117744a81a3
lilpacy <revivedtomorrow@gmail.com>
2022-12-31 08:33:59 +0900

    copy index page from prev portfolio

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,70 @@
 import './style.scss'
-import typescriptLogo from './typescript.svg'
-import {setupCounter} from './counter'
+import './index.scss'
+import zennIcon from './zenn.svg'
+import qiitaIcon from './qiita.svg'
+import pagesIcon from './githubpages.svg'
+import wpIcon from './wordpress.svg'
+import twitterIcon from './twitter.svg'
+import githubIcon from './github.svg'
+import vrchatIcon from './vrchat.svg'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div class="flex flex-wrap">
-    <div class="w-full flex justify-center items-center">
-      <a href="https://vitejs.dev" target="_blank">
-        <img src="/vite.svg" class="logo" alt="Vite logo" />
-      </a>
-      <a href="https://www.typescriptlang.org/" target="_blank">
-        <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-      </a>
+  <div class="w-full flex flex-col gap-10 justify-center items-center">
+    <h1>Hello Happy World!</h1>
+
+    <canvas id="canvas"></canvas>
+
+    <div class="flex gap-2">
+      <a href="https://zenn.dev/lilpacy"><img class="icon" src="${zennIcon}" /></a> 
+      <a href="https://qiita.com/lilpacy"><img class="icon" src="${qiitaIcon}" /></a>
+      <a href="https://lilpacy.github.io/"><img class="icon" src="${pagesIcon}" /></a> 
+      <a href="http://lilpacy.info/"><img class="icon" src="${wpIcon}" /></a>
+      <a href="https://twitter.com/lilpacy_"><img class="icon" src="${twitterIcon}" /></a>
+      <a href="https://github.com/lilpacy"><img class="icon" src="${githubIcon}" /></a>
+      <a href="https://vrchat.com/home/user/usr_7d9e5881-5bff-48fb-aa13-53415ba074ac"><img class="icon" src="${vrchatIcon}" /></a>
+      <!--  <a href="https://www.wantedly.com/id/tomohiroseto"><img class="icon" src="wantedly.png" /></a>-->
+      <!-- <a href="https://note.com/lilpacy"><img class="icon" src="note.png" /></a> -->
     </div>
-    <h1 class="text-3xl font-bold underline w-full">Vite + TypeScript</h1>
-    <div class="card w-full">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs w-full">
-      Click on the Vite and TypeScript logos to learn more
+    <p>
+      <strong>
+        Please tap a button to get further information.
+      </strong>
     </p>
   </div>
 `
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+window.onload = () => {
+  let canvas: HTMLCanvasElement|null = document.getElementById("canvas") as HTMLCanvasElement;
+  const ctx:  CanvasRenderingContext2D | null = canvas.getContext("2d");
+  let frame: number;
+
+  (function loop() {
+    frame = requestAnimationFrame(loop);
+
+    const imageData = ctx?.getImageData(0, 0, canvas.width, canvas.height);
+
+    if(!imageData) return;
+    for (let p = 0; p < imageData.data.length; p += 4) {
+      const i = p / 4;
+      const x = i % canvas.width;
+      const y = (i / canvas.height) >>> 0;
+
+      const t = window.performance.now();
+
+      const r = 64 + (128 * x) / canvas.width + 64 * Math.sin(t / 1000);
+      const g = 64 + (128 * y) / canvas.height + 64 * Math.cos(t / 1000);
+      const b = 128;
+
+      imageData.data[p + 0] = r;
+      imageData.data[p + 1] = g;
+      imageData.data[p + 2] = b;
+      imageData.data[p + 3] = 255;
+    }
+
+    ctx?.putImageData(imageData, 0, 0);
+  })();
+
+  return () => {
+    cancelAnimationFrame(frame);
+  };
+};
